506a4e2daecf93bcd85938af45c8dcdf2c8a4e81
Filip Dugandzic <alfitec@gmail.com>
2022-09-05 01:27:21 +0200

    added display of number that generates longest seq

--- project14/longestCollatz.py
+++ project14/longestCollatz.py
@@ -12,14 +12,16 @@ def CollatzLength(n):
     if n%2==0: return CollatzLength(n/2)+1
     else:      return CollatzLength(3*n+1)+1
 
-
+print(CollatzLength(13))
 
 if __name__=='__main__':
     starttime=time.perf_counter_ns()
     longestCollatz=1
+    numberForLngestSequence=1
     for i in range(1,1000001):
         tempRes=CollatzLength(i)
         if tempRes>longestCollatz:
             longestCollatz=tempRes
+            numberForLngestSequence=i
     endtime=time.perf_counter_ns()
-    print(f"{longestCollatz=}     time={endtime-starttime}")
+    print(f"{longestCollatz=}     {numberForLngestSequence=}   time={endtime-starttime}")
