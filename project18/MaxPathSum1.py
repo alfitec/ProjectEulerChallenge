7e9c2bda3e4dbbf99ee6fc609cad9705ae784677
Filip Dugandzic <alfitec@gmail.com>
2022-09-08 00:20:15 +0200

    problem18 works but requires hevy refacoring

---
+++ project18/MaxPathSum1.py
@@ -0,0 +1,32 @@
+from functools import reduce
+
+triangle=([75],
+(95,64),
+(17,47,82),
+(18,35,87,10),
+(20,4,82,47,65),
+(19,1,23,75,3,34),
+(88,2,77,73,7,63,67),
+(99,65,4,28,6,16,70,92),
+(41,41,26,56,83,40,80,70,33),
+(41,48,72,33,47,32,37,16,94,29),
+(53,71,44,65,25,43,91,52,97,51,14),
+(70,11,33,28,77,73,17,78,39,68,17,57),
+(91,71,52,38,17,14,91,43,58,50,27,29,48),
+(63,66,4,68,89,53,67,30,73,16,69,87,40,31),
+(4,62,98,27,23,9,70,98,73,93,38,53,60,4,23))
+
+def consolidateRows(shortRow,longRow):
+    print(list(zip((0,*shortRow),longRow)))
+    nwRow1=[sum(z) for z in zip((0,*shortRow),(longRow))]
+    nwRow2=[sum(z) for z in zip((*shortRow,0),(longRow))]
+    print(list((z,max(z)) for z in zip(nwRow1,nwRow2)))
+    nwRow1=(sum(z) for z in zip((0,*shortRow),(longRow)))
+    nwRow2=(sum(z) for z in zip((*shortRow,0),(longRow)))
+    return list(max(z) for z in zip(nwRow1,nwRow2))
+
+print(list(reduce(consolidateRows,((1,),(2,3),(1,1,1)))))
+#((41,41,26,56,83,40,80,70,33),
+#(41,48,72,33,47,32,37,16,94,29)))))
+print(triangle)
+print(max(list(reduce(consolidateRows,triangle)))) # [list(t) for t in triangle])))
