cb613c5c3d443e71b1e8a144a5c742b9be4bfcba
Filip Dugandzic <alfitec@gmail.com>
2022-09-09 00:43:52 +0200

    finished problem 67

--- project67/MaxPathSum2.py
+++ project67/MaxPathSum2.py
@@ -1,20 +1,18 @@
 def consolidateRows(prevRow,currentRow):
-    nwRow1=(sum(z) for z in zip((0,*prevRow),currentRow))
-    nwRow2=(sum(z) for z in zip((*prevRow,0),currentRow))
-    return tuple(max(z) for z in zip(nwRow1,nwRow2))   # returning generators without converting to touple makes problems
+    return tuple(   max(z[0],z[2])+z[1]     for z in zip(  (0,*prevRow),  currentRow,  (*prevRow,0)  )  )  
+    # returning generators without converting to touple makes problems
 
-#print(max(list(reduce(consolidateRows,triangle)))) 
-
-from functools import reduce
 import urllib.request
 
-get_url= urllib.request.urlopen('https://projecteuler.net/project/resources/p067_triangle.txt')
+"""
+with urllib.request.urlopen('https://projecteuler.net/project/resources/p067_triangle.txt') as response:
+    data=response.read().decode('utf-8')
+    triangle=[[int(y) for y in x.split()] for x in data.split("\n") if x]
+"""
 
-print("Response Status: "+ str(get_url.getcode()) )
-print(type(get_url))
-data=get_url.read().decode('utf-8')
-print(type(data))
+with open('project67/p067_triangle.txt') as triangle_file:
+    data=triangle_file.read()
+    triangle=[[int(y) for y in x.split()] for x in data.split("\n") if x]
 
-data2=[[int(y) for y in x.split()] for x in data.split("\n")]
-print(data2)
-print(max(list(reduce(consolidateRows,data2[0:-1]))))
+from functools import reduce
+print(max(list(reduce(consolidateRows,triangle))))
